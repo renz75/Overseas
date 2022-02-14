95c46e5c47c1ecb00d8e24a71112d76e831f98c7
Renzo Lopez <renzolopez@Renzos-MBP.lan>
2022-02-14 14:40:52 -0800

    Initial commit

---
+++ app/src/main/java/example/com/overseas/MainActivity.java
@@ -0,0 +1,62 @@
+package example.com.overseas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button mButton, cmButton;
+        final EditText amountEditText;
+        final TextView resultTextView;
+
+
+
+        mButton = (Button) findViewById(R.id.mButton);
+        cmButton = (Button) findViewById(R.id.cmButton);
+
+        amountEditText = (EditText) findViewById(R.id.amountEditText);
+        resultTextView = (TextView) findViewById(R.id.resultTextView);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                Double convert = Double.parseDouble(amountEditText.getText().toString());
+                resultTextView.setText(String.valueOf(convert*0.3048));
+
+                resultTextView.setTextColor(Color.RED);
+            }
+        });
+
+        cmButton.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                Double convert = Double.parseDouble(amountEditText.getText().toString());
+                resultTextView.setText(String.valueOf(convert*30.48));
+
+                resultTextView.setTextColor(Color.RED);
+            }
+        });
+
+    } // end of onCreate
+
+
+
+    }// end of main class
