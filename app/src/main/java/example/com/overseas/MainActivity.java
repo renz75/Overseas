6ee457097b4bf87633776645e8dce30fe729ba89
renz75 <renzjav@gmail.com>
2022-02-16 13:52:22 -0800

    Added a resource XML for tablet

--- app/src/main/java/example/com/overseas/MainActivity.java
+++ app/src/main/java/example/com/overseas/MainActivity.java
@@ -3,6 +3,8 @@ package example.com.overseas;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -13,15 +15,31 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private boolean phoneDevice = true; // used to force landscape mode
+    Button mButton, cmButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button mButton, cmButton;
         final EditText amountEditText;
         final TextView resultTextView;
+
+        // determine screen size
+        int screenSize = getResources().getConfiguration().screenLayout &
+                Configuration.SCREENLAYOUT_SIZE_MASK;
+
+        // if device is a tablet, set phoneDevice to false
+        if(screenSize == Configuration.SCREENLAYOUT_SIZE_LARGE ||
+        screenSize == Configuration.SCREENLAYOUT_SIZE_XLARGE)
+            phoneDevice = false; // not a phone-sized device
+
+        // if running on phone-sized device, allow only portrait orientation
+        if(phoneDevice){
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
 
 
 
