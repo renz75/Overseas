0aba68ce4513c16635d660d1e3e4a312056bd79a
renz75 <renzjav@gmail.com>
2022-02-16 16:50:36 -0800

    Added a resource XML for tablet

--- app/src/main/java/example/com/overseas/MainActivity.java
+++ app/src/main/java/example/com/overseas/MainActivity.java
@@ -3,8 +3,6 @@ package example.com.overseas;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-import android.content.pm.ActivityInfo;
-import android.content.res.Configuration;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -14,9 +12,8 @@ import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    private boolean phoneDevice = true; // used to force landscape mode
     Button mButton, cmButton;
+
 
 
     @Override
@@ -24,23 +21,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         final EditText amountEditText;
         final TextView resultTextView;
-
-        // determine screen size
-        int screenSize = getResources().getConfiguration().screenLayout &
-                Configuration.SCREENLAYOUT_SIZE_MASK;
-
-        // if device is a tablet, set phoneDevice to false
-        if(screenSize == Configuration.SCREENLAYOUT_SIZE_LARGE ||
-        screenSize == Configuration.SCREENLAYOUT_SIZE_XLARGE)
-            phoneDevice = false; // not a phone-sized device
-
-        // if running on phone-sized device, allow only portrait orientation
-        if(phoneDevice){
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        }
-
 
 
         mButton = (Button) findViewById(R.id.mButton);
@@ -56,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                Double convert = Double.parseDouble(amountEditText.getText().toString());
+                double convert = Double.parseDouble(amountEditText.getText().toString());
                 resultTextView.setText(String.valueOf(convert*0.3048));
 
                 resultTextView.setTextColor(Color.RED);
@@ -68,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                Double convert = Double.parseDouble(amountEditText.getText().toString());
+                double convert = Double.parseDouble(amountEditText.getText().toString());
                 resultTextView.setText(String.valueOf(convert*30.48));
 
                 resultTextView.setTextColor(Color.RED);
@@ -77,6 +60,4 @@ public class MainActivity extends AppCompatActivity {
 
     } // end of onCreate
 
-
-
-    }// end of main class
+}// end of main class
